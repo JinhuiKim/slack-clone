02680bb415e6c6df4d35446d255d9d8ebd995c9e
journey <public.jinhui.kim@gmail.com>
2023-01-16 01:20:54 +0900

    chore: ts + webpack 실행하기

--- front/client.tsx
+++ front/client.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './layouts/App';
 
-const root = ReactDOM.createRoot(document.querySelector('#app'));
+const root = ReactDOM.createRoot(document.querySelector('#app') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
